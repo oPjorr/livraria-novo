1ab4f30d152d15b761d60124322a06523f3ad0a9
Paulo <paulorolinski.ifc@gmail.com>
2022-07-13 14:42:23 -0300

    first commit

--- livraria/urls.py
+++ livraria/urls.py
@@ -6,9 +6,9 @@ from rest_framework.routers import DefaultRouter
 from core.views import CategoriaViewSet
 
 router = DefaultRouter()
-router.register(r'categorias', CategoriaViewSet)
+router.register(r"categorias", CategoriaViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    path("admin/", admin.site.urls),
+    path("", include(router.urls)),
 ]
